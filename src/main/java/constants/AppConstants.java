3cf2697d15b4de151536e003dea2582e89568ffe
Pavel <nichitapavel@live.com>
2016-05-16 23:27:20 +0100

    Add constants for template manipulation

---
+++ src/main/java/constants/AppConstants.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package constants;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+public class AppConstants {
+    public static final String TEMPLATE = "template";
+
+    public static final String ATTRIBUTES = "attributes";
+    public static final String ATTRIBUTES_ADD_FORM = "attributes-add-form";
+    public static final String ATTRIBUTES_LIST_CHECKBOX = "attributes-list-checkbox";
+    public static final String ATTRIBUTES_LIST_CHECKBOX_ANT = "attributes-list-checkbox-ant";
+    public static final String ATTRIBUTES_LIST_CHECKBOX_CON = "attributes-list-checkbox-con";
+    public static final String ATTRIBUTES_LIST = "attributes-list";
+    
+    public static final String FD = "fd";
+    public static final String FD_LIST = "fd-list";
+    
+    public static final String ANTECEDENT = "__ant__";
+    public static final String CONSEQUENT = "__con__";
+
+    private AppConstants() {
+        // Private constructor to prevent instantiation.
+    }
+}
---
+++ src/main/java/constants/TemplateConstants.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package constants;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+public class TemplateConstants {
+    public static final String ATTRIBUTE = "templates/attribute/attribute.vtl";
+    public static final String ATTRIBUTES_ADD_FORM = "templates/attribute/attributes-add-form.vtl";
+    public static final String ATTRIBUTES_LIST_CHECKBOX_ANT = "templates/attribute/attributes-list-checkbox-ant.vtl";
+    public static final String ATTRIBUTES_LIST_CHECKBOX_CON = "templates/attribute/attributes-list-checkbox-con.vtl";
+    public static final String ATTRIBUTES_LIST_CHECKBOX = "templates/attribute/attributes-list-checkbox.vtl";
+    public static final String ATTRIBUTES_LIST = "templates/attribute/attributes-list.vtl";
+    
+    public static final String FD = "templates/fd/fd.vtl";
+    public static final String FD_LIST = "templates/fd/fd-list.vtl";
+
+    private TemplateConstants() {
+        // Private constructor to prevent instantiation.
+    }
+}
