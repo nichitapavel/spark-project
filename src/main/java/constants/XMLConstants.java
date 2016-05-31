e08e803cd9638d6944551f015fee81ffa84d6f48
Pavel <nichitapavel@live.com>
2016-05-31 02:09:41 +0100

    Add XMLConstants with variable used in xml >
    Add XMLConstants with variable used in xml transformation

---
+++ src/main/java/constants/XMLConstants.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package constants;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+public class XMLConstants {
+
+    public static final String ATTRIBUTE = "Attribute";
+    public static final String ATTRIBUTES = "Attributes";
+
+    public static final String FD = "FD";
+    public static final String FDS = "FDs";
+    public static final String ANTECEDENT = "Antecedent";
+    public static final String CONSEQUENT = "Consequent";
+
+    public static final String FDJOINT = "FDJoint";
+    public static final String FDJOINTS = "FDJoints";
+    
+    public static final String RELATION = "Relation";
+    public static final String RELATIONS = "Relations";
+    
+    public static final String NAME = "Name";
+    public static final String UTF_8 = "UTF-8";
+    public static final String YES = "yes";
+    public static final String XML = "xml";
+    public static final String INDENT_AMOUNT = "{http://xml.apache.org/xslt}indent-amount";
+    public static final String FOUR = "4";
+    public static final String SESSION = "Session";
+    
+    public static final String ORG_ECLIPSE_JETTY_MULTIPART_CONFIG = "org.eclipse.jetty.multipartConfig";
+    public static final String TEMP = "/temp";
+
+    private XMLConstants() {
+        // Private constructor to prevent instantiation.
+    }
+}
