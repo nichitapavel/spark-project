d504b546edf6455eb93a31568afe230eb58a8151
Pavel <nichitapavel@live.com>
2016-05-31 02:10:07 +0100

    Add Routes Constants.

---
+++ src/main/java/constants/RoutesConstants.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package constants;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+public class RoutesConstants {
+    public static final String FILE_LOCATION = "/public";
+    public static final String ROOT = "/";
+    public static final String HOME = "/home";
+    public static final String SESSION = "/session";
+    public static final String ATTRIBUTE = "/attribute";
+    public static final String SAVE = "/save";
+    public static final String LOAD = "/load";
+    public static final String FD = "/fd";
+    public static final String FDJOINT = "/fdjoint";
+    public static final String RELATION = "/relation";
+    public static final String ULLMAN = "/ullman";
+    public static final String CALCULATE_KEYS = "/calculate-keys";
+    public static final String CALCULATE_MINIMAL_COVER = "/calculate-minimal-cover";
+    public static final String PROJECTION = "/projection";
+    public static final String FD_PARTOF_FDJOINT = "/fd-partof-fdjoint";
+    public static final String IMPLIES = "/implies";
+    public static final String EQUIVALENCE = "/equivalence";
+    public static final String TEST_NORMAL_FORM = "/test-normal-form";
+    public static final String TEST_KEYS = "/test-keys";
+    public static final String TEST_MINIMAL_COVER = "/test-minimal-cover";
+    public static final String NORMALIZE = "/normalize";
+    public static final String E401 = "/401";
+    public static final String DELETE_ATTRIBUTE = "/delete-attribute";
+    public static final String DELETE_FD = "/delete-fd";
+    public static final String DELETE_FDJOINT = "/delete-fdjoint";
+    public static final String DELETE_RELATION = "/delete-relation";
+    
+    private RoutesConstants() {
+        // Private constructor to prevent instantiation.
+    }
+}
