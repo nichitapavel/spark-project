b5756d0d7be984752963fbc7bd7d05c042741bf9
Pavel <nichitapavel@live.com>
2016-06-09 02:28:09 +0100

    Full functional esit/dbdstudio

--- src/main/java/constants/RoutesConstants.java
+++ src/main/java/constants/RoutesConstants.java
@@ -9,32 +9,32 @@ package constants;
  */
 public class RoutesConstants {
     public static final String FILE_LOCATION = "/public";
-    public static final String ROOT = "/odn";
-    public static final String ABOUT = "/odn/about";
-    public static final String GLOBAL_VIEW = "/odn/global-view";
-    public static final String SESSION = "/odn/session";
-    public static final String ATTRIBUTE = "/odn/attribute";
-    public static final String SAVE = "/odn/save";
-    public static final String LOAD = "/odn/load";
-    public static final String FD = "/odn/fd";
-    public static final String FDJOINT = "/odn/fdjoint";
-    public static final String RELATION = "/odn/relation";
-    public static final String ULLMAN = "/odn/ullman";
-    public static final String CALCULATE_KEYS = "/odn/calculate-keys";
-    public static final String CALCULATE_MINIMAL_COVER = "/odn/calculate-minimal-cover";
-    public static final String PROJECTION = "/odn/projection";
-    public static final String FD_PARTOF_FDJOINT = "/odn/fd-partof-fdjoint";
-    public static final String TEST_IMPLIES = "/odn/test-implies";
-    public static final String EQUIVALENCE = "/odn/equivalence";
-    public static final String TEST_NORMAL_FORM = "/odn/test-normal-form";
-    public static final String TEST_KEYS = "/odn/test-keys";
-    public static final String TEST_MINIMAL_COVER = "/odn/test-minimal-cover";
-    public static final String NORMALIZE = "/odn/normalize";
-    public static final String E401 = "/odn/401";
-    public static final String DELETE_ATTRIBUTE = "/odn/delete-attribute";
-    public static final String DELETE_FD = "/odn/delete-fd";
-    public static final String DELETE_FDJOINT = "/odn/delete-fdjoint";
-    public static final String DELETE_RELATION = "/odn/delete-relation";
+    public static final String ROOT = "/esit/dbdstudio";
+    public static final String ABOUT = "/esit/dbdstudio/about";
+    public static final String GLOBAL_VIEW = "/esit/dbdstudio/global-view";
+    public static final String SESSION = "/esit/dbdstudio/session";
+    public static final String ATTRIBUTE = "/esit/dbdstudio/attribute";
+    public static final String SAVE = "/esit/dbdstudio/save";
+    public static final String LOAD = "/esit/dbdstudio/load";
+    public static final String FD = "/esit/dbdstudio/fd";
+    public static final String FDJOINT = "/esit/dbdstudio/fdjoint";
+    public static final String RELATION = "/esit/dbdstudio/relation";
+    public static final String ULLMAN = "/esit/dbdstudio/ullman";
+    public static final String CALCULATE_KEYS = "/esit/dbdstudio/calculate-keys";
+    public static final String CALCULATE_MINIMAL_COVER = "/esit/dbdstudio/calculate-minimal-cover";
+    public static final String PROJECTION = "/esit/dbdstudio/projection";
+    public static final String FD_PARTOF_FDJOINT = "/esit/dbdstudio/fd-partof-fdjoint";
+    public static final String TEST_IMPLIES = "/esit/dbdstudio/test-implies";
+    public static final String EQUIVALENCE = "/esit/dbdstudio/equivalence";
+    public static final String TEST_NORMAL_FORM = "/esit/dbdstudio/test-normal-form";
+    public static final String TEST_KEYS = "/esit/dbdstudio/test-keys";
+    public static final String TEST_MINIMAL_COVER = "/esit/dbdstudio/test-minimal-cover";
+    public static final String NORMALIZE = "/esit/dbdstudio/normalize";
+    public static final String E401 = "/esit/dbdstudio/401";
+    public static final String DELETE_ATTRIBUTE = "/esit/dbdstudio/delete-attribute";
+    public static final String DELETE_FD = "/esit/dbdstudio/delete-fd";
+    public static final String DELETE_FDJOINT = "/esit/dbdstudio/delete-fdjoint";
+    public static final String DELETE_RELATION = "/esit/dbdstudio/delete-relation";
     
     private RoutesConstants() {
         // Private constructor to prevent instantiation.
