a1d2aefc4c5d519e2ab04380067d94a491523ab8
Pavel <nichitapavel@live.com>
2016-06-12 20:48:47 +0100

    Save calculated fdset minimal cover

--- src/main/java/constants/SessionConstants.java
+++ src/main/java/constants/SessionConstants.java
@@ -14,6 +14,7 @@ public class SessionConstants {
     public static final String RELATION_LIST = "relationList";
     public static final String USERNAME = "username";
     public static final String RELATION_NORMALIZED = "normalized";
+    public static final String FDSET_MIN = "fdSetMinimal";
 
     private SessionConstants() {
         // Private constructor to prevent instantiation.
