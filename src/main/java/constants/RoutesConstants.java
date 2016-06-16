27f5e2a55c911351136f0c4e584705c1038752c8
Pavel <nichitapavel@live.com>
2016-06-16 06:48:11 +0100

    Work with routes

--- src/main/java/constants/RoutesConstants.java
+++ src/main/java/constants/RoutesConstants.java
@@ -8,35 +8,35 @@ package constants;
  *
  */
 public class RoutesConstants {
-    public static final String FILE_LOCATION = "/public";
-    public static final String ROOT = "odn";
-    public static final String ABOUT = "about";
-    public static final String GLOBAL_VIEW = "global-view";
-    public static final String SESSION = "session";
-    public static final String ATTRIBUTE = "attribute";
-    public static final String SAVE = "save";
-    public static final String LOAD = "load";
-    public static final String FD = "fd";
-    public static final String FDJOINT = "fdjoint";
-    public static final String RELATION = "relation";
-    public static final String ADD_RELATION = "add-relation";
-    public static final String ULLMAN = "ullman";
-    public static final String CALCULATE_KEYS = "calculate-keys";
-    public static final String CALCULATE_MINIMAL_COVER = "calculate-minimal-cover";
-    public static final String PROJECTION = "projection";
-    public static final String FD_PARTOF_FDJOINT = "fd-partof-fdjoint";
-    public static final String TEST_IMPLIES = "test-implies";
-    public static final String EQUIVALENCE = "equivalence";
-    public static final String TEST_NORMAL_FORM = "test-normal-form";
-    public static final String TEST_KEYS = "test-keys";
-    public static final String TEST_MINIMAL_COVER = "test-minimal-cover";
-    public static final String NORMALIZE = "normalize";
-    public static final String E401 = "401";
-    public static final String DELETE_ATTRIBUTE = "delete-attribute";
-    public static final String DELETE_FD = "delete-fd";
-    public static final String DELETE_FDJOINT = "delete-fdjoint";
-    public static final String DELETE_RELATION = "delete-relation";
-    public static final String ADD_FDSET = "add-fdjoint";
+    public static final String FILE_LOCATION = "public";
+    public static final String ROOT = "odn/home";
+    public static final String ABOUT = "odn/about";
+    public static final String GLOBAL_VIEW = "odn/global-view";
+    public static final String SESSION = "odn/session";
+    public static final String ATTRIBUTE = "/attribute";
+    public static final String SAVE = "odn/save";
+    public static final String LOAD = "odn/load";
+    public static final String FD = "odn/fd";
+    public static final String FDJOINT = "odn/fdjoint";
+    public static final String RELATION = "odn/relation";
+    public static final String ADD_RELATION = "odn/add-relation";
+    public static final String ULLMAN = "odn/ullman";
+    public static final String CALCULATE_KEYS = "odn/calculate-keys";
+    public static final String CALCULATE_MINIMAL_COVER = "odn/calculate-minimal-cover";
+    public static final String PROJECTION = "odn/projection";
+    public static final String FD_PARTOF_FDJOINT = "odn/fd-partof-fdjoint";
+    public static final String TEST_IMPLIES = "odn/test-implies";
+    public static final String EQUIVALENCE = "odn/equivalence";
+    public static final String TEST_NORMAL_FORM = "odn/test-normal-form";
+    public static final String TEST_KEYS = "odn/test-keys";
+    public static final String TEST_MINIMAL_COVER = "odn/test-minimal-cover";
+    public static final String NORMALIZE = "odn/normalize";
+    public static final String E401 = "odn/401";
+    public static final String DELETE_ATTRIBUTE = "odn/delete-attribute";
+    public static final String DELETE_FD = "odn/delete-fd";
+    public static final String DELETE_FDJOINT = "odn/delete-fdjoint";
+    public static final String DELETE_RELATION = "odn/delete-relation";
+    public static final String ADD_FDSET = "odn/add-fdjoint";
     
     private RoutesConstants() {
         // Private constructor to prevent instantiation.
